2f2687132f944d73413673da424ee3142a29e7ab
ambrociojosec <ambrocio.josec@gmail.com>
2019-05-07 00:32:58 -0700

    defined some javascript functions

--- js/main.js
+++ js/main.js
@@ -2,12 +2,32 @@
 
 
 /*----- app's state (variables) -----*/
-
+let winner, score, btn;
 
 /*----- cached element references -----*/
 
-
 /*----- event listeners -----*/
-
+btn = document.querySelector('button');
+btn.addEventListener('click', play);
 
 /*----- functions -----*/
+init();
+
+function play(event) {
+	console.log(event);
+}
+
+function render() {
+
+}
+
+function init() {
+	//will be set to somethig
+	winner = null;
+	score = {
+		image1: 'images/1.png',
+		image2: 'images/2.png',
+		image3: 'images/3.png'
+	}
+	render()
+}
